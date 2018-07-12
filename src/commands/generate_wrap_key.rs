e0e24e6931d8d5d4ce2ec4ee596a709a4564e8e5
harrier <mmu20046f03@yahoo.com.hk>
2018-07-12 13:55:56 +0800

    Rewrite generate_wrap_key and fix typo

--- src/commands/generate_wrap_key.rs
+++ src/commands/generate_wrap_key.rs
@@ -2,54 +2,66 @@
 //!
 //! <https://developers.yubico.com/YubiHSM2/Commands/Generate_Wrap_Key.html>
 
-/*
+use super::{Command, Response};
+use {
+    CommandType, Capability, Connector, Domain, ObjectId, ObjectLabel, 
+    Session, SessionError, WrapAlgorithm,
+};
 
-    /// Generate wrapkey
-    pub fn generate_wrap_key(
-        &mut self,
-        key_id: ObjectId,
-        label: ObjectLabel,
-        domains: Domains,
-        capabilities: Capabilities,
-        algorithm: Algorithm,
-        dc: Capabilities,
-    ) -> Result<GenerateWrapKeyResponse, SessionError>
-    {
-        self.send_encrypted_command(GenerateWrapKeyCommand {
-            key_id,
-            label,
-            domains,
-            capabilities,
-            algorithm,
-            dc,
-        })
-    }
+/// Generate a new wrap key within the `YubiHSM2`
+pub fn generate_wrap_key<C: Connector>(
+    session: &mut Session<C>,
+    key_id: ObjectId,
+    label: ObjectLabel,
+    domains: Domain,
+    capabilities: Capability,
+    algorithm: WrapAlgorithm,
+    delegated_capabilities: Capability,
+) -> Result<GenerateWrapKeyResponse, SessionError> {
+    session.send_encrypted_command(GenerateWrapKeyCommand {
+        key_id,
+        label,
+        domains,
+        capabilities,
+        algorithm,
+        delegated_capabilities,
+    })
+}
 
-/// Response from `CommandType::GenerateWrapKey`
+
+/// Request parameters for `commands::generate_wrap_key`
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct GenerateWrapKeyCommand {
+    /// ID of the key
+    pub key_id: ObjectId,
+
+    /// Label for the key (40-bytes)
+    pub label: ObjectLabel,
+
+    /// Domain in which the key will be accessible
+    pub domains: Domain,
+
+    /// Capability of the key
+    pub capabilities: Capability,
+
+    /// Key algorithm
+    pub algorithm: WrapAlgorithm,
+
+    /// Capability of object import/export by the key
+    pub delegated_capabilities: Capability,
+}
+
+impl Command for GenerateWrapKeyCommand {
+    type ResponseType = GenerateWrapKeyResponse;
+}
+
+/// Response from `commands::generate_wrap_key`
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GenerateWrapKeyResponse {
-    /// ID of the key to be generated
+    /// ID of the key
     pub key_id: ObjectId,
 }
 
 impl Response for GenerateWrapKeyResponse {
     const COMMAND_TYPE: CommandType = CommandType::GenerateWrapKey;
 }
-
-/// Request parameters for `CommandType::GenerateWrapKey`
-/// 
-/// <https://developers.yubico.com/YubiHSM2/Commands/Generate_Wrap_Key.html>
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GenerateWrapKeyCommand {
-    pub key_id: ObjectId,
-    pub label: ObjectLabel,
-    pub domains: Domains,
-    pub capabilities: Capabilities,
-    pub algorithm: Algorithm,
-    pub dc: Capabilities,
-}
-
-impl Command for GenerateWrapKeyCommand {
-    type ResponseType = GenerateWrapKeyResponse;
-}
-*/
